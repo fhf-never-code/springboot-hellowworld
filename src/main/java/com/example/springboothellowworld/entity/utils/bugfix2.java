523e2bfb6d88ce75591746a08139db598ab92aaf
y.f.li <502796125@qq.com>
2020-03-09 19:31:41 +0800

    bugfix2 commit fifth commit

--- src/main/java/com/example/springboothellowworld/entity/utils/bugfix2.java
+++ src/main/java/com/example/springboothellowworld/entity/utils/bugfix2.java
@@ -1,10 +1,12 @@
 package com.example.springboothellowworld.entity.utils;
 
+import java.sql.SQLOutput;
 import java.util.function.Function;
 
 public class bugfix2 {
     public void sbumit(){}
     //增加注解
-    public void sbumit2(){}
-
+    public void sbumit2(){
+    }
 }
+
