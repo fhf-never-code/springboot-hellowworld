7543ccaf603f2f8018160c17055778a35ac5c75e
y.f.li <502796125@qq.com>
2020-03-09 18:05:35 +0800

    bugfix2 commit 4nd final

---
+++ src/main/java/com/example/springboothellowworld/entity/utils/bugfix2.java
@@ -0,0 +1,10 @@
+package com.example.springboothellowworld.entity.utils;
+
+import java.util.function.Function;
+
+public class bugfix2 {
+    public void sbumit(){}
+    //增加注解
+    public void sbumit2(){}
+
+}
