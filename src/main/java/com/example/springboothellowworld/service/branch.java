b99395dd0bef9e678d99c3fe2bb5d85312966d03
y.f.li <502796125@qq.com>
2020-03-09 17:36:47 +0800

    bugfix commit

--- src/main/java/com/example/springboothellowworld/service/branch.java
+++ src/main/java/com/example/springboothellowworld/service/branch.java
@@ -3,6 +3,9 @@ package com.example.springboothellowworld.service;
 public class branch {
     public void add(){
         int x = 10;
-        int y = 10/0;
+        int y = 10/x;
+    }
+    public void fix(){
+
     }
 }
